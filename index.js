60d6bb825e5e6c238971fc1c2ee4e3b1267d5ce0
Eric <eriqthedev@gmail.com>
2021-08-24 13:03:30 +0300

    Final commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-function start(){
-   console.log('Started ...');
+function start() {
+  console.log("Started ...");
 }
-function test(){
-  console.log('test..');
+function test() {
+  console.log("test..");
 }
-
+//Add two numbers
+function sum(a, b) {
+  const sum = a + b;
+  console.log(sum);
+  return sum;
+}
 start();
-
+sum(4, 5);
