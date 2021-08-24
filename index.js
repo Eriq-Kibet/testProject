e6d93e09c5f92827a4363a178b38c485ecdacb93
Eric Kibet <83347293+Eriq-Kibet@users.noreply.github.com>
2021-08-24 16:48:03 +0300

    Final Commit (#2)

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ function test() {
   console.log("test..");
 }
 //Add two numbers
-function sum(a, b) {
+const sum = (a, b) => {
   const sum = a + b;
   console.log(sum);
   return sum;
-}
+};
+
 start();
 sum(4, 5);
---
+++ subtract.js
@@ -0,0 +1,9 @@
+// this is a second file
+const subtract = (x, y) => {
+  const subtract = y - x;
+  console.log(subtract);
+  return subtract;
+}
+subtract(8, 50);
+
+
